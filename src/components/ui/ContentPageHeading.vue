878d1f25c433bbe5414f0e7bd5ce1254aa182f07
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-10-21 09:37:15 -0400

    Add styles and slots to Content Page Heading

--- src/components/ui/ContentPageHeading.vue
+++ src/components/ui/ContentPageHeading.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <div class="border-b border-brand-green-300 pb-3">
-      <h1 class="text-8xl text-brand-green-800"><slot name="header"></slot></h1>
-      <h1><slot name="byline"></slot></h1>
+    <div class="mt-20 mb-20">
+      <!-- Heading -->
+      <h1 class="text-8xl text-brand-green-800">
+        <slot name="header">No Header Provided</slot>
+      </h1>
+      <!-- Markup byline only if content is provided in the slot -->
+      <p
+        v-if="$slots['byline']"
+        class="mt-2 w-full md:w-4/5 lg:w-3/5 text-3xl text-brand-green-600"
+      >
+        <slot name="byline"></slot>
+      </p>
     </div>
   </div>
 </template>
