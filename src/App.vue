62207ff0fc2a567861aef6ce11d25bae9df4b008
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-10-21 00:50:22 -0400

    Fix typescript build error

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,7 @@
 <script lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+export default {
+  setup() {},
+};
 </script>
