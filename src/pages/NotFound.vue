c00577b9497e80a4d3ff9a91353fb2b115145a59
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-11-11 16:50:12 -0500

    Initial attempt at error page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,25 +1,50 @@
 <template>
   <div>
-    <layout>
-      <content-container>
-        <p>Not Found</p>
-      </content-container>
-    </layout>
+    <page-layout>
+      <div class="mt-20 mb-20">
+        <div class="flex flex-col md:flex-row-reverse">
+          <div class="md:w-1/2">
+            <img :src="errorImageSvgUrl" alt="" />
+          </div>
+          <div class="md:w-1/2">
+            <h1 class="text-8xl text-brand-green-800">
+              Oh No!<span class="sr-only"> Error</span>
+            </h1>
+            <p
+              class="
+                mt-2
+                w-full
+                md:w-4/5
+                lg:w-3/5
+                text-3xl text-brand-green-600
+              "
+            >
+              Something went horribly wrong.
+            </p>
+          </div>
+        </div>
+      </div>
+    </page-layout>
   </div>
 </template>
 
 <script lang="ts">
-import Layout from '../layouts/Default.vue';
+import PageLayout from '../layouts/Page.vue';
 import ContentContainer from '../components/ui/ContentContainer.vue';
+import errorImageSvgUrl from '../assets/images/error.svg';
+
 import { useHead } from '@vueuse/head';
 
 export default {
   components: {
     ContentContainer,
-    Layout,
+    PageLayout,
   },
   setup() {
     setupHeadTags();
+    return {
+      errorImageSvgUrl,
+    };
   },
 };
 
