51b70e83b5dd22d8fa64555754598a5c3af44f4b
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-11-12 08:51:57 -0500

    Add mobile error page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -3,12 +3,14 @@
     <page-layout>
       <div class="mt-20 mb-20">
         <div class="flex flex-col md:flex-row-reverse">
-          <div class="md:w-1/2">
-            <img :src="errorImageSvgUrl" alt="" />
+          <div class="md:w-1/3">
+            <div class="pb-20 pr-10 md:pt-0 md:px-0">
+              <img :src="errorImageSvgUrl" alt="" />
+            </div>
           </div>
-          <div class="md:w-1/2">
+          <div class="md:w-2/3">
             <h1 class="text-8xl text-brand-green-800">
-              Oh No!<span class="sr-only"> Error</span>
+              <span class="sr-only">Error</span> Oops!
             </h1>
             <p
               class="
@@ -19,8 +21,26 @@
                 text-3xl text-brand-green-600
               "
             >
-              Something went horribly wrong.
+              Looks like either this page is missing or some sort of
+              catastrophic error occured.
             </p>
+
+            <p class="mt-10 text-xl">
+              Check out our home page or reach out using our contact page.
+            </p>
+
+            <ul class="mt-10 flex flex-row justify-between">
+              <li class="w-1/2 pr-2">
+                <router-link :to="{ name: 'home' }" class="btn-primary w-full">
+                  <span class="sr-only">Go to the FemDev</span> Home page
+                </router-link>
+              </li>
+              <li class="w-1/2 pl-2">
+                <router-link :to="{ name: 'home' }" class="btn-primary w-full">
+                  <span class="sr-only">Go to the FemDev</span> Contact us
+                </router-link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
