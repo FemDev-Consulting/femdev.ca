1b3cead31b450cb39a877b06d6637ad01d221ccd
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-11-15 14:59:52 -0500

    Test netlify prod environment handling

--- src/components/layout/ThePageBanner.vue
+++ src/components/layout/ThePageBanner.vue
@@ -1,34 +1,37 @@
 <template>
-  <div class="bg-brand-green-900">
-    <div class="py-5">
-      <content-container>
-        <div class="text-brand-green-300">
-          <section>
-            <h2 class="sr-only">Test site notice</h2>
-            <div class="flex flex-nowrap items-center">
-              <div class="border rounded-md py-1 px-2">
-                <p
-                  class="
-                    text-center
-                    sm:text-left
-                    text-sm
-                    md:text-base
-                    uppercase
-                  "
-                >
-                  Test Site
-                </p>
+  <div v-if="isProduction === false">
+    <!-- Only display the page banner if we aren't in prod -->
+    <div class="bg-brand-green-900">
+      <div class="py-5">
+        <content-container>
+          <div class="text-brand-green-300">
+            <section>
+              <h2 class="sr-only">Test site notice</h2>
+              <div class="flex flex-nowrap items-center">
+                <div class="border rounded-md py-1 px-2">
+                  <p
+                    class="
+                      text-center
+                      sm:text-left
+                      text-sm
+                      md:text-base
+                      uppercase
+                    "
+                  >
+                    Test Site
+                  </p>
+                </div>
+                <div class="ml-10">
+                  <p class="text-sm">
+                    This site is in active development. Parts of this site may
+                    not work and will change.
+                  </p>
+                </div>
               </div>
-              <div class="ml-10">
-                <p class="text-sm">
-                  This site is in active development. Parts of this site may not
-                  work and will change.
-                </p>
-              </div>
-            </div>
-          </section>
-        </div>
-      </content-container>
+            </section>
+          </div>
+        </content-container>
+      </div>
     </div>
   </div>
 </template>
@@ -40,5 +43,23 @@ export default {
   components: {
     ContentContainer,
   },
+  setup() {
+    // process.env.MY_SECRET_SAUCE
+    const isProduction = useProductionFlag();
+
+    return { isProduction };
+  },
 };
+function useProductionFlag() {
+  let production: boolean = false;
+  const PROD_STRING: string = 'production';
+
+  if (import.meta.env.VITE_PROD_CONTEXT === PROD_STRING) {
+    production = true;
+  }
+
+  console.log('PROD ENV:', import.meta.env.VITE_PROD_CONTEXT, production);
+
+  return production;
+}
 </script>
